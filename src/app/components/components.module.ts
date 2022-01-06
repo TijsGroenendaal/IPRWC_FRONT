a0ece9731eeb2b1b6a94f51d9b74d9d11dabcb3b
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-01-06 16:06:25 +0100

    ✨ create paged list

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagedListComponent } from './paged-list/paged-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NgxPaginationModule } from "ngx-pagination";
 
 
 
 @NgModule({
   declarations: [
     PagedListComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxPaginationModule,
+  ],
+  providers: [],
+  bootstrap: [],
+  exports: [
+    PagedListComponent,
   ]
 })
 export class ComponentsModule { }
--- src/app/components/paged-list/paged-list.component.ts
+++ src/app/components/paged-list/paged-list.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ContentChild,
+  TemplateRef,
+} from '@angular/core';
+import { PageType } from "./page.type";
+import { PageableInterface } from "./pageable.interface";
 
 @Component({
-  selector: 'app-paged-list',
+  selector: 'app-paged-list-component',
   templateUrl: './paged-list.component.html',
-  styleUrls: ['./paged-list.component.css']
+  styleUrls: ['./paged-list.component.css'],
 })
 export class PagedListComponent implements OnInit {
+  @Input() pageableInterface: PageableInterface;
 
-  constructor() { }
+  @ContentChild('row', { static: false }) rowTemplate: TemplateRef<any>;
+
+  private _page: PageType<any>;
+  pageSize = 10;
 
   ngOnInit(): void {
+    this.getPage(0);
+    this.pageableInterface.observable.subscribe(() => {
+      this.getPage(this.page.pageable.pageNumber);
+    });
   }
 
+  handlePageChange(event: number): void {
+    this.getPage(event - 1);
+  }
+
+  private getPage(pageNumber: number): void {
+    this.pageableInterface
+      .getPage(pageNumber, this.pageSize)
+      .subscribe((page) => {
+        this.page = { ...page };
+      });
+  }
+
+  get page(): PageType<any> {
+    return this._page;
+  }
+
+  set page(value: PageType<any>) {
+    this._page = value;
+  }
 }
