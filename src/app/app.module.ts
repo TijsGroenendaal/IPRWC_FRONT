c17973bd472fb21698ea67db4fd8b53c3e9e9097
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-01-13 19:59:10 +0100

    ✨ create http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginModule} from "./login/login.module";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "./components/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -13,6 +15,12 @@ import {LoginModule} from "./login/login.module";
     LoginModule,
     BrowserModule,
     AppRoutingModule
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
   ],
   providers: [],
   bootstrap: [AppComponent]
