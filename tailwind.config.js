953a3461876a92b9b5e9ee68db08baa4925ff56a
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-01-15 17:40:55 +0100

    🔧 update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    screens: {
+      'sx': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       colors: {
         error: '#b73232',
